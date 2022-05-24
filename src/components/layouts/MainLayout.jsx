62f95921c3a8f4cd562f0356828b86fa66d02c65
Harshna N Haswani <hharshna.n@gmail.com>
2022-05-24 17:37:16 +0530

    Layout based on Auth

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useWindowDimension } from "../../context/window-context";
 import { Footer } from "../Footer";
 import { MainNav } from "../Nav/MainNav";
 
 export const MainLayout = () => {
+  const {windowDimension} = useWindowDimension();
   return (
     <div>
       <MainNav />
-      <main>
+      <main className={`${windowDimension.windowWidth > 960 ? "center-xy" : ''}`}>
         <Outlet />
       </main>
       <Footer />
