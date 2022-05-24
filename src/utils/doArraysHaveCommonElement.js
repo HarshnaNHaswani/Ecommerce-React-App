2b8eba6f5b81062adb3880f3ad309d439f598c9f
Harshna N Haswani <hharshna.n@gmail.com>
2022-05-24 12:53:15 +0530

    product-listing utils1

--- src/utils/doArraysHaveCommonElement.js
+++ src/utils/doArraysHaveCommonElement.js
@@ -1,3 +1,3 @@
 import React from 'react'
 
-export const doArraysHaveCommonElement = (arr1, arr2) =>  arr1.some(item => arr2.includes(item))
+export const doArraysHaveCommonElement = (arr1, arr2) =>  [...arr1].some(item => arr2.includes(item))
