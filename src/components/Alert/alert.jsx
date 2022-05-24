b5a67a4255e9492e63dad2b1da241463623be1f0
Harshna N Haswani <hharshna.n@gmail.com>
2022-05-24 18:02:29 +0530

    alert component

---
+++ src/components/Alert/alert.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useAlert } from "../../context/alert-context";
+import "./alert.css";
+export const Alert = () => {
+  const { alert } = useAlert();
+  const { status, text, display: displayAlert } = alert;
+
+  return (
+    <>
+      {displayAlert && (
+        <p className={`alert status-${status}-outline status-${status}-bg`}>
+          {text}
+        </p>
+      )}
+    </>
+  );
+};
