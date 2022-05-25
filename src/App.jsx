3e6b75f3cfefe60137a05b6737795d9baac7cf74
Harshna N Haswani <hharshna.n@gmail.com>
2022-05-25 12:32:58 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ function App() {
   const { showAlert } = useAlert();
   const { setIsLoggedIn } = useAuth();
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token") ?? '';
     const storedUser = JSON.parse(localStorage.getItem("user")) ?? {};
     const lengthOfStoredUser = Object.keys(storedUser)?.length ?? 0;
-    if (lengthOfStoredUser > 0 && token.trim().length > 0) {
+    if (lengthOfStoredUser > 0 && token.length > 0 ) {
       showAlert({
         text: `Welcome back ${storedUser.firstName ?? ""}!`,
         status: "success",
       });
       return;
     }
-    if (lengthOfStoredUser || token.length > 0) {
+    if (lengthOfStoredUser || token?.length > 0) {
       showAlert({
         text: "error retrieving user data, Please login",
         status: "error",
