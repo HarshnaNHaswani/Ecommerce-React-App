93f04900b1589d9131c637a7d9965b550bbaf97c
Harshna N Haswani <hharshna.n@gmail.com>
2022-05-25 12:27:39 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,27 @@ import "./App.css";
 import { Alert } from "./components/Alert/alert";
 import { useAlert } from "./context/alert-context";
 import { Router } from "./router/Router";
-import {useAuth} from "./context/auth-context"
+import { useAuth } from "./context/auth-context";
 function App() {
   const { showAlert } = useAlert();
-  const {setIsLoggedIn} = useAuth()
+  const { setIsLoggedIn } = useAuth();
   useEffect(() => {
     const token = localStorage.getItem("token");
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const lengthOfStoredUser = Object.keys(storedUser).length
+    const storedUser = JSON.parse(localStorage.getItem("user")) ?? {};
+    const lengthOfStoredUser = Object.keys(storedUser).length;
     if (lengthOfStoredUser > 0 && token.trim().length > 0) {
-      showAlert({ text: `Welcome back ${storedUser.firstName?? ''}!`, status: 'success' });
+      showAlert({
+        text: `Welcome back ${storedUser.firstName ?? ""}!`,
+        status: "success",
+      });
       return;
-    } 
-    if(lengthOfStoredUser || token.length > 0) {
-      showAlert({ text: 'error retrieving data, Please login again', status: 'error' })
-      setIsLoggedIn(false)
+    }
+    if (lengthOfStoredUser || token.length > 0) {
+      showAlert({
+        text: "error retrieving user data, Please login",
+        status: "error",
+      });
+      setIsLoggedIn(false);
     }
   }, []);
 
