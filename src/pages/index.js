621361b7feeaab4852ceb160eadbdebd06166bca
Harshna N Haswani <hharshna.n@gmail.com>
2022-08-17 15:50:15 +0530

    update: pages index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,7 @@ export { Home } from "./home/Home";
 export { Wishlist } from "./wishlist";
 export { Login, SignUp, ForgotPassword } from "./auth";
 export { Cart } from "./cart";
-export { ProductListing } from "./products";
+export { ProductListing  } from "./products";
 export { NotFound } from "./notFound";
+export {ProductCrashFallback} from "./products/ProductCrashFallback"
+export { SingleProduct } from "./products/SingleProduct";
