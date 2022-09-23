4d0218fabee398dc2ce1cd030deecd4af202ca85
Harshna N Haswani <hharshna.n@gmail.com>
2022-09-23 13:52:04 +0530

    refactor: id for db

--- src/backend/db/coupons.js
+++ src/backend/db/coupons.js
@@ -2,14 +2,14 @@ import { v4 as uuid } from "uuid";
 
 export const coupons = [
   {
-    _id: uuid(),
+    _id: 'coupon-1',
     couponDescription: "50% off",
     coupon: "50",
     couponCode: "SHOP50",
     maxDiscount: "450"
   },
   {
-    _id: uuid(),
+    _id: 'coupon-2',
     couponDescription: "30% off",
     coupon: "30",
     couponCode: "SHOP30",
